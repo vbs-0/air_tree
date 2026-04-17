7b318c36a5396ba81e4029119d27fe83e492ca91
vbs-0 <chbhadri0@gmail.com>
2026-04-17 09:14:50 +0530

    lineage_air: Harmonize product name and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_air.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_air-bp4a-user \
-    lineage_air-bp4a-userdebug \
-    lineage_air-bp4a-eng
+    lineage_air-user \
+    lineage_air-userdebug \
+    lineage_air-eng
